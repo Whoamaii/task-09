b7c4a2d35ba4616a3e7ef1e760cfe757be39adec
Whoamaii <110328724+Whoamaii@users.noreply.github.com>
2022-12-06 09:38:51 +0200

    Update scrypt.js

--- scrypt.js
+++ scrypt.js
@@ -1,10 +1,10 @@
 const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
 
 function generateKey(arr, len) {
-	arr = arr.split('')
+  arr = arr.split('')
   let a = arr.map((i) => [Math.random(), i]).sort().map((i) => i[1]);
   a.length = len;
-  return a.join("");
+  	return a.join("");
  
 }
 console.log(generateKey(characters, 16));
